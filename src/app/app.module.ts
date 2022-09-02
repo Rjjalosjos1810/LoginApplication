88c87ff50cd5e8908fff508bf5c1175ba2491df2
Rj Jalosjos <rjjalosjoss@gmail.com>
2022-09-02 17:37:21 +0800

    added additional depedencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,17 @@ import {CardModule} from 'primeng/card';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
 import { FormsModule } from '@angular/forms';
+import {TabMenuModule} from 'primeng/tabmenu';
+import {SplitterModule} from 'primeng/splitter';
+import {DividerModule} from 'primeng/divider';
+
+import {TableModule} from 'primeng/table';
+import { CustomerService } from './customer.service';
+import { HttpClientModule } from '@angular/common/http';
+import {FileUploadModule} from 'primeng/fileupload';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DropdownModule} from 'primeng/dropdown';
+
 
 @NgModule({
   declarations: [			
@@ -24,9 +35,19 @@ import { FormsModule } from '@angular/forms';
     CardModule,
     InputTextModule,
     ButtonModule,
-    FormsModule
+    FormsModule,
+    TabMenuModule,
+    SplitterModule,
+    DividerModule,
+    TableModule,
+    HttpClientModule,
+    FileUploadModule,
+    BrowserAnimationsModule,
+    DropdownModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CustomerService],
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
