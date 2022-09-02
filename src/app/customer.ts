0aa88a56112c4046ce325c85ca261f73e81a366a
Rj Jalosjos <rjjalosjoss@gmail.com>
2022-09-02 17:36:53 +0800

    added customer service for the table graph

---
+++ src/app/customer.ts
@@ -0,0 +1,12 @@
+export interface Customer {
+    id?: number;
+    firstname?: string;
+    lastname?: string;
+    country?: string;
+    nationality?: string;
+    company?: string;
+    designation?: string;
+    workexperience?: string;
+    cv?: string;
+    datasource?: string;
+}
